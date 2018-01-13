0059db0c6f6cbc46a524c8f82c53b4a93e008f3a
shenyuanping <shenyuanping@eunke.com>
2018-01-13 18:25:35 +0800

    #spring cloud config server

---
+++ spring-cloud-config-git/spring-cloud-config-server/src/main/java/com.neo/ConfigServerApplication.java
@@ -0,0 +1,16 @@
+package com.neo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * Created by sheny on 2018/1/13.
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+}
