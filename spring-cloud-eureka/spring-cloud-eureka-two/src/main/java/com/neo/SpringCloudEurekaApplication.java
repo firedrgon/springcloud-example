adf0cb92ba8e1b6ac2f8ac9f6684737ed8b1fe1f
shenyuanping <shenyuanping@eunke.com>
2018-01-13 14:19:58 +0800

    #spring-cloud-eureka

---
+++ spring-cloud-eureka/spring-cloud-eureka-two/src/main/java/com/neo/SpringCloudEurekaApplication.java
@@ -0,0 +1,16 @@
+package com.neo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Created by sheny on 2018/1/13.
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class SpringCloudEurekaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudEurekaApplication.class, args);
+    }
+}
